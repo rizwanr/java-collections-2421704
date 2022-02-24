65f338f5716e166fc4ce95fe37417ee73ff40624
rizwanr <rizwanrenesa@gmail.com>
2022-02-24 15:50:54 +0600

    chanllenge_Iterating_Collections

--- src/main/java/com/linkedin/collections/RoomService.java
+++ src/main/java/com/linkedin/collections/RoomService.java
@@ -17,22 +17,27 @@ public class RoomService {
 	public void applyDiscount(final double discount) {
 		
 		//Reduces the rate of each room by the provided discount
+
+		inventory.stream()
+				.forEach(room -> room.setRate(room.getRate()* (1-discount)));
+
 	
 	}
 
 	public Collection<Room> getRoomsByCapacity(final int requiredCapacity) {
 		
 		//Returns a new collection of rooms that meet or exceed the provided capacity
+		Collection<Room> roomsByCapacity = inventory.stream().filter(room -> room.getCapacity()>= requiredCapacity).collect(Collectors.toList());
 		
-		return null;
+		return roomsByCapacity;
 		
 	}
 	
 	public Collection<Room> getRoomByRateAndType(final double rate, final String type){
 	
 		//Returns a new collection of rooms with a rate below the provided rate and that match the provided type
-		
-		return null;
+		Collection<Room> roomsByRateAndType= inventory.stream().filter(room -> room.getRate()<rate).filter(room ->room.getType().equals(type)).collect(Collectors.toList());
+		return roomsByRateAndType;
 		
 	}
 	
