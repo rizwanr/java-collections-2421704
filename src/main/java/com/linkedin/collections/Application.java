6a6404acdf74e061efef8e76aae3396b51291da6
rizwanr <rizwanrenesa@gmail.com>
2022-02-27 16:06:12 +0600

    queue implentation

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,8 @@
 package com.linkedin.collections;
 
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Application {
@@ -11,6 +14,38 @@ public class Application {
 		Guest sonia = new Guest("Sonia", "Doe", true); //loyalty program
 		Guest siri = new Guest("Siri", "Doe", true); //loyalty program
 
+		// Queue is first in, first out, FIFO
+
+		//difference between poll and peek is poll removes the element from the queue, peek does not.
+
+		Queue<Guest> checkinQueue0 = new ArrayDeque<>();
+		checkinQueue0.offer(john);
+		checkinQueue0.offer(bob);
+		print(checkinQueue0);
+
+		Guest guest0 = checkinQueue0.poll();
+		print(checkinQueue0);
+		Guest guest1 = checkinQueue0.peek();
+		print(checkinQueue0);
+		System.out.println(guest0);
+
+		Comparator<Guest> programComp
+				= Comparator.comparing(Guest::isLoyaltyProgramMember).reversed();
+
+		Queue<Guest> checkinQueue = new PriorityQueue<>(programComp);
+		checkinQueue.add(john);
+		checkinQueue.add(bob);
+		checkinQueue.add(sonia);
+		checkinQueue.add(siri);
+		print(checkinQueue);
+
+		Guest guest = checkinQueue.remove();
+		print(checkinQueue);
+		System.out.println(guest);
+
+		Guest guest2 = checkinQueue.element();
+		print(checkinQueue);
+		System.out.println(guest1);
 
 
 	}
